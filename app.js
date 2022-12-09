24149e921c08bb26410e51b3376327d927349b4e
Zefanja Timorason <z.timorason@gmail.com>
2022-12-09 15:28:43 +0100

    finished the lab

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
@@ -6,6 +7,14 @@ app.use(express.static("public"));
 
 app.get("/home", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/catmemes", (req, res) => {
+  res.sendFile(__dirname + "/views/catmemes.html");
 });
 
 app.listen(3001, () => console.log("connected"));
