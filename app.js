7b387f6a6e6f235b577134cf17489d2044aa43f9
Zefanja Timorason <z.timorason@gmail.com>
2022-12-09 14:56:44 +0100

    made the first webpage

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require("express");
 
-app.get('/home', (request, response, next) => {
-    console.log(request);
-    response.send('<h1>Welcome Ironhacker. :)</h1>');
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/home", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
 });
-app.listen(3000, () => console.log('My first app listening on port 3000! '));
+
+app.listen(3001, () => console.log("connected"));
+console.log(__dirname);
